efb157b6078e16e51e7f028b9b138c2aacf974ac
yichengjie <626659321@qq.com>
2015-08-21 22:44:22 +0800

    scope_&的使用，传递的是父scope的方法

--- filter/index.js
+++ filter/index.js
@@ -1,7 +1,17 @@
 var app = angular.module("myApp",[]) ;
 
+app.controller('IndexController',function($scope){
+    $scope.userList = [
+        {"name":"yicj","addr":"beijing"},
+        {"name":"caobei","addr":"zhengzhou"},
+        {"name":"wangjie","addr":"neimenggu"},
+        {"name":"wanghao","addr":"beijing"}
+    ] ;
+    $scope.key = "" ;
+}) ;
+
 app.filter("myFilter", function() {
-    return function(data,key){
+    var myFunc = function(data,key){
         var retData = [] ;
         if(key.length>0){
             angular.forEach(data,function(e){
@@ -14,17 +24,18 @@ app.filter("myFilter", function() {
         }
         return retData ;
     }
+    return myFunc ;
 });
 
-app.controller('IndexController',function($scope){
-    $scope.userList = [
-        {"name":"yicj","addr":"beijing"},
-        {"name":"caobei","addr":"zhengzhou"},
-        {"name":"wangjie","addr":"neimenggu"},
-        {"name":"wanghao","addr":"beijing"}
-    ] ;
-    $scope.key = "" ;
-}) ;
+app.filter('capitalize', function() {
+    return function(input) {
+        if (input) {
+            return input[0].toUpperCase() + input.slice(1);
+        }
+    }
+});
+
+
 
 
 
